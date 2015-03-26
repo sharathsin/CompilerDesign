8f37851e1bff374a09f9d32057102d5caef9c675
s_jillel <s_jillel@MURAD.ENCS.concordia.ca>
2015-03-26 15:34:57 -0400

    en

--- ScannerLex/src/Typechecking/StatBlock.java
+++ ScannerLex/src/Typechecking/StatBlock.java
@@ -4,5 +4,24 @@ import java.util.ArrayList;
 
 public class StatBlock {
 	ArrayList<Statment> s;
+	public boolean  evaluate()
+	{
+		boolean b=true;
+	for(Statment s1:s)
+	{
+	if(s1 instanceof ForStatement)
+	{
+		if(! ((ForStatement) s1).isValid())
+		{
+			b=false;
+		}
+	}
+	else{
+		if(! ((returnStatement) s1).evaulate())b=false;
+	}
 
+
+	}
+	return b;
+	}
 }
--- ScannerLex/src/Typechecking/StatementList.java
+++ ScannerLex/src/Typechecking/StatementList.java
@@ -4,7 +4,28 @@ import java.util.ArrayList;
 
 public class StatementList {
 
-ArrayList<Statment>list;
+ArrayList<Statment>s;
 String function,classname;
+public boolean  evaluate()
+{
+	boolean b=true;
+for(Statment s1:s)
+{
+if(s1 instanceof ForStatement)
+{
+	if(! ((ForStatement) s1).isValid())
+	{
+		b=false;
+	}
+}
+else{
+	if(! ((returnStatement) s1).evaulate())b=false;
+}
+
+
+}
+return b;
+}
+
 
 }
