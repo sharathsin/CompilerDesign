07204154593ff57b00976c0443ebeff5699e3de5
s_jillel <s_jillel@MURAD.ENCS.concordia.ca>
2015-03-26 16:10:05 -0400

    mistakes

--- ScannerLex/src/Typechecking/getStatement.java
+++ ScannerLex/src/Typechecking/getStatement.java
@@ -1,5 +1,9 @@
 package Typechecking;
 
 public class getStatement implements Statment {
-
+	Variable e;
+	public boolean validate()
+	{
+		return !e.evaluate().equals("Invalid");
+	}
 }
--- ScannerLex/src/Typechecking/putStatement.java
+++ ScannerLex/src/Typechecking/putStatement.java
@@ -1,5 +1,10 @@
 package Typechecking;
 
 public class putStatement implements Statment {
+	Expression e;
+	public boolean validate()
+	{
+		return !e.evaluate().equals("Invalid");
+	}
 
 }
